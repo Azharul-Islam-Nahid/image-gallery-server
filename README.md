0b6ba3c2860ef75b12648ace8c20a750706a7d88
Azharul Islam <lonewolfazharul@gmail.com>
2023-11-01 01:30:12 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# b6a11-service-review-server-side-Azharul-Islam-Nahid
+
+This server is created for generating portraits tourer client site data.
+
+# for data fetch post delete update mongodb server used.
+
+# 6 categories of api created for data fetching purpose.
+
+# 3 categories of api added from data posting purpose.
+
+# 1 delete method and update method added from mongo for data delete and updating.
+
+# JWT token verification system added for database security purpose.
+
+# Data deployed in vercel for availability.
---
+++ index.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+// MongoDB connection URI
+const uri = "mongodb+srv://imagegallery:gYMzyZ3KjPYALzCy@cluster0.jgjsyco.mongodb.net/?retryWrites=true&w=majority";
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+
+    client.connect();
+
+    console.log("Connected to the MongoDB database");
+
+    const imageCollection = client.db("imageGallery").collection("images");
+
+    app.get("/", (req, res) => {
+      res.send("Image gallery server is running");
+    });
+
+    app.get("/allimages", async (req, res) => {
+      const query = {};
+      const cursor = imageCollection.find(query);
+      const images = await cursor.toArray();
+      res.send(images);
+    });
+
+    app.post("/image", async (req, res) => {
+      const service = req.body;
+      const result = await imageCollection.insertOne(service);
+      res.send(result);
+    });
+
+    app.delete('/review/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await imageCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    app.listen(port, () => {
+      console.log(`Image gallery server running on ${port}`);
+    });
+  } catch (error) {
+    console.error("Error: ", error);
+  } finally {
+
+    await client.close();
+  }
+}
+
+run().catch((err) => console.error(err));
